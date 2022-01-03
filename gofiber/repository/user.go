5362d4d9d84e2a7951fc70a82633c34505546f00
Chayut Ruksomya <chayut.r@bluepi.co.th>
2022-01-03 22:47:41 +0700

    initial user repository

--- gofiber/repository/user.go
+++ gofiber/repository/user.go
@@ -1,1 +1,13 @@
 package repository
+
+type User struct {
+	username    string `db:"username"`
+	password    string `db:"password"`
+	role        string `db:"role"`
+	customer_id int8   `db:"customer_id"`
+}
+
+type UserRepository interface {
+	Create(User) error
+	GetByUsername(string) (*User, error)
+}
--- gofiber/repository/user_db.go
+++ gofiber/repository/user_db.go
@@ -1,1 +1,21 @@
 package repository
+
+import "github.com/jmoiron/sqlx"
+
+type userRespositoryDB struct {
+	db *sqlx.DB
+}
+
+// NOTE : Constructure
+func NewUserRepositoryDB(db *sqlx.DB) userRespositoryDB {
+	return userRespositoryDB{db: db}
+}
+
+// TODO : Implment from UserRepository interface
+func (r userRespositoryDB) Create(user User) error {
+	return nil
+}
+
+func (r userRespositoryDB) GetByUsername(userName string) (*User, error) {
+	return nil, nil
+}

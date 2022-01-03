b62a1bfe185514ecc4f441b5f50974bf5fc70e65
Chayut Ruksomya <chayut.r@bluepi.co.th>
2022-01-03 15:19:08 +0700

    add params

--- gofiber/main.go
+++ gofiber/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -10,13 +11,18 @@ import (
 func main() {
 	// NOTE : Using Fiber Web Framework
 
-	app := fiber.New(fiber.Config{
-		Prefork: true,
-	})
+	app := fiber.New()
 
-	app.Get("/hello/string", func(c *fiber.Ctx) error {
+	app.Get("/hello", func(c *fiber.Ctx) error {
 		// NOTE : Inline handler function
 		return c.SendString("Hello World!")
+	})
+
+	app.Get("/hello/:first_name/:last_name?", func(c *fiber.Ctx) error {
+		// NOTE : Get params with optional + default
+		fName := c.Params("first_name", "default_first_name")
+		lName := c.Params("last_name", "default_last_name")
+		return c.SendString(fmt.Sprintf("fn = %v | ln = %v", fName, lName))
 	})
 
 	app.Listen(":8000")
